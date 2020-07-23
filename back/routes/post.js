2919354e73339e461b33c0bf02aa1ef3a4322e54
SONG22 <annna0449@naver.com>
2020-07-23 17:36:43 +0900

    multer s3

--- back/routes/post.js
+++ back/routes/post.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path"); //node module
 const fs = require("fs");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
 
 const { Post, User, Image, Comment, Hashtag } = require("../models");
 const { isLoggedIn } = require("./middleware");
@@ -15,18 +17,31 @@ try {
   fs.mkdirSync("uploads");
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, "uploads");
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname); // 확장자 추출 .png
-      const basename = path.basename(file.originalname, ext); // 파일 이름
-
-      done(null, basename + new Date().getTime() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: "cheering99",
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
   }),
+
+  // multer.diskStorage({
+  //   destination(req, file, done) {
+  //     done(null, "uploads");
+  //   },
+  //   filename(req, file, done) {
+  //     const ext = path.extname(file.originalname); // 확장자 추출 .png
+  //     const basename = path.basename(file.originalname, ext); // 파일 이름
+
+  //     done(null, basename + new Date().getTime() + ext);
+  //   },
+  // }),
   limits: { fileSize: 20 * 1024 * 1024 }, // 20MB
 });
 
@@ -314,7 +329,7 @@ router.delete("/:postId/unlike", isLoggedIn, async (req, res, next) => {
 router.post("/images", isLoggedIn, upload.array("image"), (req, res, next) => {
   // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.filename));
+  res.json(req.files.map((v) => v.location));
 });
 
 router.post("/:postId/retweet", isLoggedIn, async (req, res, next) => {
