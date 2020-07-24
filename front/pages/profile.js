38312f71b3270b1c74a187c493b4bcbba7547d1e
SONG22 <annna0449@naver.com>
2020-07-24 18:54:13 +0900

    front profile fix

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,82 +1,65 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/react-in-jsx-scope */
-import AppLayout from "../components/AppLayout";
+import React, { useEffect, useState, useCallback } from "react";
 import Head from "next/head";
+import { useSelector } from "react-redux";
+import Router from "next/router";
+import { END } from "redux-saga";
+import axios from "axios";
+import useSWR from "swr";
+
+import AppLayout from "../components/AppLayout";
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState, useCallback } from "react";
-import Router from "next/router";
-import {
-  LOAD_FOLLOWINGS_REQUEST,
-  LOAD_FOLLOWERS_REQUEST,
-  LOAD_MY_INFO_REQUEST,
-} from "../reducers/user";
+import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
 import wrapper from "../store/configureStore";
-import axios from "axios";
-import { END } from "redux-saga";
-import useSWR from "swr";
 import { backUrl } from "../config/config";
 
 const fetcher = (url) =>
   axios.get(url, { withCredentials: true }).then((result) => result.data);
-//fetcher를 다른 걸로 바꾸면 graphQL도 쓸 수 있음
+
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
   const [followersLimit, setFollowersLimit] = useState(3);
   const [followingsLimit, setFollowingsLimit] = useState(3);
+
   const { data: followersData, error: followerError } = useSWR(
     `${backUrl}/user/followers?limit=${followersLimit}`,
     fetcher,
-  ); //fetcher : 주소를 어떻게 가져올지, data, error 가없으면 loading
-
+  );
   const { data: followingsData, error: followingError } = useSWR(
     `${backUrl}/user/followings?limit=${followingsLimit}`,
     fetcher,
   );
 
   useEffect(() => {
-    dispatch({
-      type: LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: LOAD_FOLLOWINGS_REQUEST,
-    });
-  }, []);
-
-  useEffect(() => {
     if (!(me && me.id)) {
       Router.push("/");
     }
   }, [me && me.id]);
-  if (!me) {
-    return null;
-  }
 
   const loadMoreFollowings = useCallback(() => {
     setFollowingsLimit((prev) => prev + 3);
   }, []);
+
   const loadMoreFollowers = useCallback(() => {
     setFollowersLimit((prev) => prev + 3);
   }, []);
 
   if (!me) {
-    return <div> 내정보 로딩중...</div>;
+    return "내 정보 로딩중...";
   }
 
   if (followerError || followingError) {
     console.error(followerError || followingError);
-    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>; //여기서 리턴하면 useEffect 가 실행이 안됨.훅스는 모두 실행이 돼야함
+    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>;
   }
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <NicknameEditForm></NicknameEditForm>
+        <NicknameEditForm />
         <FollowList
           header="팔로잉"
           data={followingsData}
@@ -93,8 +76,11 @@ const Profile = () => {
     </>
   );
 };
+
 export const getServerSideProps = wrapper.getServerSideProps(
   async (context) => {
+    console.log("getServerSideProps start");
+    console.log(context.req.headers);
     const cookie = context.req ? context.req.headers.cookie : "";
     axios.defaults.headers.Cookie = "";
     if (context.req && cookie) {
@@ -104,8 +90,9 @@ export const getServerSideProps = wrapper.getServerSideProps(
       type: LOAD_MY_INFO_REQUEST,
     });
     context.store.dispatch(END);
+    console.log("getServerSideProps end");
     await context.store.sagaTask.toPromise();
   },
-); // 화면을 그리기전에 서버쪽에서 먼저 실행
+);
 
 export default Profile;
